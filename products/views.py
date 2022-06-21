690a64b6a90864be724d6cba54d4e1c9001506ca
GitHub-Harrison <harrison-johnson@outlook.com>
2022-06-21 16:02:19 +0000

    Create products view, admin, models and products.html page

--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
-def all_packages(request):
+
+def all_products(request):
     """ a view to show all coaching packages, sorting and searching """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
 
     return render(request, 'products/products.html', context)
