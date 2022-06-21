9a40ef06e8cbb54d2c02069ecba3e1069b69e04d
GitHub-Harrison <harrison-johnson@outlook.com>
2022-06-21 14:47:34 +0000

    Add product to installed apps and debug=true

--- dream_coaching/settings.py
+++ dream_coaching/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['dream-coaching.herokuapp.com', 'localhost']
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
