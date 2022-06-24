d02d2b4c5b6fd583ac3de060bb0c8f60a7e51d03
GitHub-Harrison <harrison-johnson@outlook.com>
2022-06-24 11:30:21 +0000

    Remove sorting via name

--- products/views.py
+++ products/views.py
@@ -20,9 +20,6 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
