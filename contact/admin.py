b1f3696c3c9368ceca29e79c3bdb46534d2b5194
GitHub-Harrison <harrison-johnson@outlook.com>
2022-06-28 14:12:46 +0000

    Add contact app with working views/urls/models/templates - not saving to admin

---
+++ contact/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Contact
+
+# Register your models here.
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'message',
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
---
+++ contact/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Contact
+
+
+def contact_us(request):
+    if request.method == 'POST':
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save
+
+    return render(request, 'contact/contact.html')
