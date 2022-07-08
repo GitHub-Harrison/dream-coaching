53f95d0c92e5c39bd204eb50e23aa87fa531064b
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-08 16:50:28 +0000

    Add if statements to allow items to be added to bag + messages for succesful

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -12,9 +12,18 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add an item to the shopping bag """
 
-    redirect_url = request.POST.get('redirect_url')
+    product = get_object_or_404(Product, pk=item_id)
     bag = request.session.get('bag', {})
+    if 'date' in request.POST:
+        date = request.POST.get('date')
 
+    if date:
+        bag[item_id] = {'date': date}
+    else:
+        bag[item_id] = item_id
+
+    messages.success(request, 'added to bag')
+    redirect_url = request.POST.get('redirect_url')
     request.session['bag'] = bag
 
     return redirect(redirect_url)
