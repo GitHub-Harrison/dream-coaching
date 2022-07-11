a432999ba1d1b36666535676d7cc2395bf1f2398
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-11 14:42:26 +0000

    Add comments to checkout models for future changes

--- checkout/models.py
+++ checkout/models.py
@@ -16,7 +16,7 @@ class Order(models.Model):
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)  # do i need grand total if each item only has one quantity?
     original_bag = models.TextField(null=False, blank=False, default='')
     # Add stripe pid
 
@@ -49,7 +49,7 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
+    quantity = models.IntegerField(null=False, blank=False, default=0)  # is this quantity for number of items in bag or quantity of same items
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
