85781878660cb57de34b768a77a4edd6cd3225b3
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-11 13:27:00 +0000

    Fix spelling mistake

--- dream_coaching/settings.py
+++ dream_coaching/settings.py
@@ -182,7 +182,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storage.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
