fea2657591484d927fbfaf526c2f59b6026f9531
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-12 14:18:16 +0000

    Add forms, signals, apps for checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',)  # potentially adding billing address
+
+    super().__init__(*args, **kwargs)
+    placeholders = {
+        'full_name': 'Full Name',
+        'email': 'Email Address',
+        'phone_number': 'Phone Number',
+    }
+
+    self.fields['full_name'].widget.attrs['autofocus'] = True
+    for field in self.fields:
+        if self.fields[field].required:
+            placeholder = f'{placeholders[field]} *'
+        else:
+            placeholder = placeholders[field]
+        self.fields[field].widget.attrs['placeholder'] = placeholder
+        self.fields[field].widget.attrs['class'] = ''  # to add css styling later
+        self.fields[field].label = False  # removes the default labels
