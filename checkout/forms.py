d71062b537af79af38fc17d385f083d0b422769c
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-13 12:20:04 +0000

    Create/update checkout forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -9,19 +9,24 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = ('full_name', 'email', 'phone_number',)  # potentially adding billing address
 
-    super().__init__(*args, **kwargs)
-    placeholders = {
-        'full_name': 'Full Name',
-        'email': 'Email Address',
-        'phone_number': 'Phone Number',
-    }
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+        }
 
-    self.fields['full_name'].widget.attrs['autofocus'] = True
-    for field in self.fields:
-        if self.fields[field].required:
-            placeholder = f'{placeholders[field]} *'
-        else:
-            placeholder = placeholders[field]
-        self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] = ''  # to add css styling later
-        self.fields[field].label = False  # removes the default labels
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = ''  # to add css styling later
+            self.fields[field].label = False  # removes the default labels
