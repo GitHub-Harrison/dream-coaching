f8a0f779fb524d377bf747b1f02503ad3a20bfb6
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-15 13:02:52 +0000

    Remove sku field from products model

--- products/models.py
+++ products/models.py
@@ -21,7 +21,6 @@ class Product(models.Model):
     description = models.TextField()
     image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    sku = models.CharField(max_length=200, null=True, blank=True)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     duration = models.DurationField()
 
