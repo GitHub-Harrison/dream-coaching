b5063c5a75478b1b4b000fdfe32957982a431e90
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-15 15:27:00 +0000

    Add success/error message to contact form for ux

--- contact/views.py
+++ contact/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 from .models import Contact
 
 
 def contact_us(request):
     if request.method == 'POST':
-        contact = Contact()
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+        if form.is_valid():
+            contact = Contact()
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
 
-        contact.name = name
-        contact.email = email
-        contact.message = message
-        contact.save()
+            contact.name = name
+            contact.email = email
+            contact.message = message
+            contact.save()
+            messages.success(request, 'Message sent successfully')
+        else:
+            messages.error(request, 'Message failed to send')
 
     return render(request, 'contact/contact.html')
