264415761737fd47d8239b4e5059983e8d14818e
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-15 14:54:12 +0000

    Add stripe for stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L15njAZKpHm33ruRjqqMhRwofp0FZiWzsZrJbowkBx2ylXlNs47WizPRqYE0rQyZk2U9KDSvpbjAfbLLrxxPcXu001tOM1825',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
