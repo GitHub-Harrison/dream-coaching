93fe5b0449fa82a520a693f5b6fb3db786ec07a7
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-17 13:36:38 +0000

    Add stripe variables to settings.py

--- dream_coaching/settings.py
+++ dream_coaching/settings.py
@@ -202,4 +202,9 @@ if 'USE_AWS' in os.environ:
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# Stripe
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
