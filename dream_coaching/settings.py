d5200019f750f2a5a390ccd5489f033534173fdb
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-21 10:34:33 +0000

    Create user profile app/model

--- dream_coaching/settings.py
+++ dream_coaching/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'bag',
     'contact',
     'checkout',
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ profiles/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def profile(request):
+    """ display user profile """
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
