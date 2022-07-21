96ea20e550a1c71cbd27a27ceba7363af470a38d
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-21 15:38:34 +0000

    Add def __str__ to newsletter model

--- home/models.py
+++ home/models.py
@@ -5,3 +5,6 @@ class NewsletterUser(models.Model):
     """ simple newlesletter model """
     email = models.EmailField()
     date_subscribed = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
