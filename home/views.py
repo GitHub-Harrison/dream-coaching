fd5d9a8dff4fe0cac2754d6921b81341e5729dbd
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-21 16:02:13 +0000

    Create newsletter views

--- home/views.py
+++ home/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from .models import NewsletterUser
+from .forms import NewsletterSignUpForm
 
 
 def index(request):
     """ a view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def newsletter_signup(request):
+    form = NewsletterSignUpForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterUser.objects.filter(email=instance.email).exists():
+            messages.info(request, 'It appears this email is already subscribed to our newsletter!')
+        else:
+            messages.success(request, 'Your email has been successfully added to our newsletter list!')
+            instance.save()
+
+    template = 'home/index.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
