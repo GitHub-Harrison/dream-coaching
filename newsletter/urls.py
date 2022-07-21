fd92478d5ba52f87980cd9cc145bf21f4be85cdc
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-21 16:31:59 +0000

    Add newsletter to navbar, correct newsletter urls

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.newsletter_signup, name='newsletter_signup'),
+    path('', views.newsletter_signup, name='newsletter'),
 ]
