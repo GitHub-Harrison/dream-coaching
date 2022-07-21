175b4c76b78e97a36f46af059dea4f2871aae2df
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-21 11:38:38 +0000

    Update webhook handler for user profile creation/saving

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -34,6 +35,16 @@ class StripeWH_Handler:
         billing_details = intent.charges.data[0].billing_details
         grand_total = round(intent.charges.data[0].amount / 100, 2)
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.defualt_full_name = billing_details.name
+                profile.defualt_email = billing_details.email
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -61,6 +72,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=billing_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     grand_total=grand_total,
                     original_bag=bag,
