63a41ebd8d4aea202612e5ce782ce916c5083f3f
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-22 16:07:43 +0000

    Remove quantity from orderlineitem model

--- checkout/models.py
+++ checkout/models.py
@@ -50,7 +50,6 @@ class Order(models.Model):
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
@@ -58,7 +57,7 @@ class OrderLineItem(models.Model):
         override the original save method to set the lineitem total
         and update the order total
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_total = self.product.price
         super().save(*args, **kwargs)
 
     def __str__(self):
