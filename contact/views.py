20afcdcf76d93acb7eaa15bf81bb24f765ceb9f7
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-25 16:29:30 +0000

    Fix form not defined contact form error

--- contact/views.py
+++ contact/views.py
@@ -2,19 +2,14 @@ from django.shortcuts import render
 from django.contrib import messages
 
 from .models import Contact
+from .forms import ContactForm
 
 
 def contact_us(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         if form.is_valid():
-            contact = Contact()
-            name = request.POST.get('name')
-            email = request.POST.get('email')
-            message = request.POST.get('message')
-
-            contact.name = name
-            contact.email = email
-            contact.message = message
+            contact = form.save(commit=False)
             contact.save()
             messages.success(request, 'Message sent successfully')
         else:
