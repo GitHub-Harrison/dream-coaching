0397374b80f632374ee4667e4f877f07dd6871f9
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-28 16:32:30 +0000

    Add heroku and local wh secret

--- dream_coaching/settings.py
+++ dream_coaching/settings.py
@@ -196,7 +196,8 @@ if 'USE_AWS' in os.environ:
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_WH_SECRET_LOCAL = os.getenv('STRIPE_WH_SECRET_LOCAL', '')
+STRIPE_WH_SECRET_HEROKU = os.getenv('STRIPE_WH_SECRET_HEROKU', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
