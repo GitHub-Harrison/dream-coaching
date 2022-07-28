92229939f16301d5be44069f101fedfdebac19ae
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-28 14:38:57 +0000

    Add remove from bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<int:item_id>/', views.add_to_bag, name='add_to_bag'),
+    path(
+        'remove/<int:item_id>/', views.remove_from_bag,
+        name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.contrib import messages
 from products.models import Product
 
@@ -27,3 +28,20 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
 
     return redirect(redirect_url)
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
