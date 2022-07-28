8a46d8c7198783fa7c03b8f239f87474eced2090
GitHub-Harrison <harrison-johnson@outlook.com>
2022-07-28 15:20:46 +0000

    Remove if statement in for loop L104

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -101,13 +101,12 @@ class StripeWH_Handler:
                 )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
